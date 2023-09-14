7a66fea97cb1ace0dd338f189df1de41f3e4bc6a
Cuongse173196 <cuongpdse173196@fpt.edu.vn>
2023-09-14 22:13:45 +0700

    feat/project-02: Navigate and create the login page

---
+++ src/view/Login/login_page.jsx
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+import LoginForm from "./login_form";
+import NavbarForHomePage from "../../Components/Navbar/navbar_for_home_page.jsx";
+
+const LoginPage = () => {
+  const [loggedInUser, setLoggedInUser] = useState(null);
+
+  const handleLogin = (username) => {
+    // This is where you update the login state
+    setLoggedInUser(username);
+  };
+
+  return (
+    <div>
+      {loggedInUser ? (
+        <div>
+          <p>Hello, {loggedInUser}!</p>
+          {/* Add logout button and logic here if needed */}
+        </div>
+      ) : (
+        <>
+          {/* <Loader></Loader> */}
+          <NavbarForHomePage isSearchVisible={false} signUp={true} />
+          <LoginForm onLogin={handleLogin} />
+        </>
+      )}
+    </div>
+  );
+};
+
+export default LoginPage;
