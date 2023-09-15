a2d25485aeae134fa1dd613dbda8d32979023906
Cuong3103 <144188187+Cuong3103@users.noreply.github.com>
2023-09-15 18:55:44 +0700

    Feat/project 05: Set up loading  (#6)
    * feat/project-05: add loading component
    
    * feat/project-05: set up loading page

---
+++ src/Components/Loading/style_component.jsx
@@ -0,0 +1,16 @@
+export const loadingContainer = {
+  width: "10rem",
+  height: "6rem",
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+  paddingTop: "20rem",
+  paddingLeft: "42rem",
+};
+export const loadingCircle = {
+  display: "block",
+  width: "1.5rem",
+  height: "1.5rem",
+  backgroundColor: "#398378",
+  borderRadius: "10rem",
+};
--- src/index.js
+++ src/index.js
@@ -1,18 +1,36 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
+import React, { useEffect, useState } from "react";
+import ReactDOM from "react-dom";
 import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 
+import Loader from "./Components/Loading/index.jsx";
 import Index from "./view/index";
 import Login from "./view/Login/login_page.jsx";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const App = () => {
+  const [isLoading, setIsLoading] = useState(true);
 
-root.render(
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" exact element={<Index />} />
-      <Route path="/login-page" exact element={<Login />} />
-      <Route path="*" element={<Navigate to="/" replace />} />
-    </Routes>
-  </BrowserRouter>
-);
+  useEffect(() => {
+    const loadingTimeout = setTimeout(() => {
+      setIsLoading(false);
+    }, 1500);
+
+    return () => {
+      clearTimeout(loadingTimeout); // Xóa timeout nếu component bị unmount
+    };
+  }, []);
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={isLoading ? <Loader /> : <Index />} />
+        <Route
+          path="/login-page"
+          element={isLoading ? <Loader /> : <Login />}
+        />
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
