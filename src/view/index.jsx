9b0abd955df4941b74482e02fba4ac35862e068c
Cuongse173196 <cuongpdse173196@fpt.edu.vn>
2023-09-15 15:47:00 +0700

    feat/project-05: set up loading page

--- src/view/index.jsx
+++ src/view/index.jsx
@@ -3,11 +3,6 @@ import React from "react";
 import NavbarForHomePage from "../Components/Navbar/navbar_for_home_page";
 
 class Index extends React.Component {
-  componentDidMount() {
-    document.documentElement.scrollTop = 0;
-    document.scrollingElement.scrollTop = 0;
-    this.refs.main.scrollTop = 0;
-  }
   render() {
     return (
       <>
