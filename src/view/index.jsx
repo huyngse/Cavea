3a19af8b4dfafd67829ce5ef4af967115a3e3b3e
Cuongse173196 <cuongpdse173196@fpt.edu.vn>
2023-09-15 09:48:12 +0700

    feat/project-03: set up for view home page

--- src/view/index.jsx
+++ src/view/index.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 
 import NavbarForHomePage from "../Components/Navbar/navbar_for_home_page";
+import Header from "../Components/Header/index.jsx";
+// import MediaCard from "../Components/Cart";
+import MenuAndSeller from "../Components/Menu-and-seller";
 
 class Index extends React.Component {
   render() {
@@ -8,6 +11,14 @@ class Index extends React.Component {
       <>
         <main ref="main"></main>
         <NavbarForHomePage isSearchVisible={true} login={true} signUp={false} />
+        <Header />
+        {/* <MediaCard
+          productName="Product 1"
+          productDescription="This is the description about the first product"
+          productImage="../images/product/product1.png"
+        /> */}
+
+        <MenuAndSeller></MenuAndSeller>
       </>
     );
   }
