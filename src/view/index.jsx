a285e49d1f3d48b31e7cdc5e5061391e8b1d4b02
Cuongse173196 <cuongpdse173196@fpt.edu.vn>
2023-09-28 20:36:54 +0700

    feat/project-11: Add link to card

--- src/view/index.jsx
+++ src/view/index.jsx
@@ -1,4 +1,5 @@
-import React, { useRef } from "react";
+import React, { useState, useRef } from "react";
+import { useNavigate } from "react-router-dom";
 
 import Navbar from "../Components/Navbar/index.jsx";
 import Header from "../Components/Header/index.jsx";
