e3972ebd7720bc0c0fcd96b077e7142ff566fe68
Cuongse173196 <cuongpdse173196@fpt.edu.vn>
2023-09-28 20:34:48 +0700

    feat/project-11: create router

--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@ import Login from "./view/Login/login_page.jsx";
 import SignUp from "./view/SignUp/signup_page.jsx";
 import BlogPage from "./view/Blog/blog_page.jsx";
 import ServicePage from "./view/Service-page/service_page.jsx";
+import ProductDetailPage from "./view/Product-detail/product_detail_page.jsx";
 
 const App = () => {
   const [isLoading, setIsLoading] = useState(true);
@@ -38,9 +39,11 @@ const App = () => {
             element={isLoading ? <Loader /> : <ServicePage />}
           />
           <Route path="/blog" element={isLoading ? <Loader /> : <BlogPage />} />
-          
+          <Route
+            path="/product-detail/:productId"
+            element={isLoading ? <Loader /> : <ProductDetailPage />}
+          />
           <Route path="*" element={<Navigate to="/" replace />} />
-          
         </Routes>
       </BrowserRouter>
     </AuthProvider>
