5c6b09255ff1b04eadc16ceb3f46f4838b0680f7
huyngse <ngogiahuy775309@gmail.com>
2023-09-28 21:53:50 +0700

    Update icon and title

--- src/view/index.jsx
+++ src/view/index.jsx
@@ -8,52 +8,62 @@ import Footer from "../Components/footer/index.jsx";
 import BackToTopButton from "../Components/BackToHome/index.jsx";
 
 import { useAuth } from "../AuthContext.jsx";
+import "./index.css"
+import Unspported from "../images/unsupported.png"
 
 const Index = () => {
   const mainRef = useRef(null);
   const { loggedInUser } = useAuth();
   return (
     <>
-      <main ref={mainRef}></main>
-      {loggedInUser ? (
-        <>
-          <Navbar avatar={true} />
-          <Header Order={true} />
-        </>
-      ) : (
-        <>
-          <Navbar isSearchVisible={true} login={true} />
-          <Header signUp={true} />
-        </>
-      )}
+      <div className="root">
+        <main ref={mainRef}></main>
+        {loggedInUser ? (
+          <>
+            <Navbar avatar={true} />
+            <Header Order={true} />
+          </>
+        ) : (
+          <>
+            <Navbar isSearchVisible={true} login={true} />
+            <Header signUp={true} />
+          </>
+        )}
 
-      <div id="phu-kien">
-        <TitleBar label="Phụ kiện lồng chim" />
+        <div id="phu-kien">
+          <TitleBar label="Phụ kiện lồng chim" />
+        </div>
+        <ListCart></ListCart>
+        <div id="cu-gay">
+          <TitleBar label="Lồng chim cu gáy"></TitleBar>
+        </div>
+        <ListCart></ListCart>
+        <div id="hoa-mi">
+          <TitleBar label="Lồng chim họa mi"></TitleBar>
+        </div>
+        <ListCart></ListCart>
+        <div id="chao-mao">
+          <TitleBar label="Lồng chim chào mào"></TitleBar>
+        </div>
+        <ListCart></ListCart>
+        <div id="khuyen">
+          <TitleBar label="Lồng chim khuyên"></TitleBar>
+        </div>
+        <ListCart></ListCart>
+        <div id="choe-than">
+          <TitleBar label="Lồng chim chòe than"></TitleBar>
+        </div>
+        <ListCart></ListCart>
+        <BackToTopButton />
+        <Footer />
       </div>
-      <ListCart></ListCart>
-      <div id="cu-gay">
-        <TitleBar label="Lồng chim cu gáy"></TitleBar>
+      <div className="unsupported-message">
+          <img src={Unspported}></img>
+          <h1>Hiện tại trang Web chưa hỗ trợ thiết bị màn hình nhỏ!</h1>
+          <p>Vui lòng sử dụng laptop hoặc máy tính để truy cập trang web này</p>
       </div>
-      <ListCart></ListCart>
-      <div id="hoa-mi">
-        <TitleBar label="Lồng chim họa mi"></TitleBar>
-      </div>
-      <ListCart></ListCart>
-      <div id="chao-mao">
-        <TitleBar label="Lồng chim chào mào"></TitleBar>
-      </div>
-      <ListCart></ListCart>
-      <div id="khuyen">
-        <TitleBar label="Lồng chim khuyên"></TitleBar>
-      </div>
-      <ListCart></ListCart>
-      <div id="choe-than">
-        <TitleBar label="Lồng chim chòe than"></TitleBar>
-      </div>
-      <ListCart></ListCart>
-      <BackToTopButton />
-      <Footer />
     </>
+
   );
 };
 
