5d2c09d4f3366f087307b9a57a98388ecfe23b6c
levquang <138362246+levquang@users.noreply.github.com>
2023-10-01 22:58:10 +0700

    Add static data (#21)
    * update more database list-product
    
    * update more database list-product

---
+++ src/Components/List-cart/list_product_chaomao.jsx
@@ -0,0 +1,160 @@
+export const products_chaomao = [
+    {
+      id: 1,
+      name: "Lồng Chào Mào Báng Súng",
+      descriptions: "CG 0350",
+      image:
+        "https://longchim.vn/wp-content/uploads/2023/05/long-chao-mao-bang-sung-1-247x296.jpg",
+      rating: 4,
+      price: "4600000",
+      discount: "3500000",
+    },
+    {
+      id: 2,
+      name: "Lồng Chào Mào Chân Trụ",
+      descriptions: "CG 01500",
+      image:
+        "https://longchim.vn/wp-content/uploads/2023/05/long-chao-mao-chan-tru-1-247x296.jpg",
+      rating: 5,
+      price: "17000000",
+      discount: "15000000",
+    },
+    {
+      id: 3,
+      name: "Lồng Chào Mào Gỗ Mun Hoa",
+      descriptions: "CG 0850",
+      image:
+        "https://longchim.vn/wp-content/uploads/2023/05/long-chao-mao-go-mun-hoa-1-247x296.jpg",
+      rating: 5,
+      price: "1100000",
+      discount: "900000",
+    },
+    {
+      id: 4,
+      name: "Lồng Chào Mào Gỗ Mun Sọc Chân Chạm",
+      descriptions: "CG350",
+      image:
+        "https://longchim.vn/wp-content/uploads/2023/05/long-chao-mao-go-mun-soc-chan-cham-1-247x296.jpg",
+      rating: 3,
+      price: "450000",
+      discount: "370000",
+    },
+    {
+        id: 5,
+        name: "Lồng Chào Mào Sin Tre Già",
+        descriptions: "CG350",
+        image:
+          "https://longchim.vn/wp-content/uploads/2023/05/long-chao-mao-sin-tre-gia-1-247x296.jpg",
+        rating: 3,
+        price: "450000",
+        discount: "3700000",
+    },
+    {
+        id: 6,
+        name: "Lồng Chào Mào Tang Lồi Cắt Góc",
+        descriptions: "CG350",
+        image:
+          "https://longchim.vn/wp-content/uploads/2023/05/long-chao-mao-tang-loi-cat-goc-1-247x296.jpg",
+        rating: 3,
+        price: "450000",
+        discount: "370000",
+    },
+    {
+        id: 7,
+        name: "Lồng Chào Mào Tang Lồi Vuông",
+        descriptions: "CG350",
+        image:
+          "https://longchim.vn/wp-content/uploads/2023/05/long-chao-mao-tang-loi-vuong-1-247x296.jpg",
+        rating: 3,
+        price: "370000",
+        discount: "450000",
+    },{
+        id: 8,
+        name: "Lồng Chào Mào Tre Già Nan Khít",
+        descriptions: "CG350",
+        image:
+          "https://longchim.vn/wp-content/uploads/2023/05/long-chao-mao-tre-gia-nan-khit-1-247x296.jpg",
+        rating: 3,
+        price: "370000",
+        discount: "450000",
+    },  {
+      id: 9,
+      name: "Lồng Chào Mào Báng Súng",
+      descriptions: "CG 0350",
+      image:
+        "https://longchim.vn/wp-content/uploads/2023/05/long-chao-mao-bang-sung-1-247x296.jpg",
+      rating: 4,
+      price: "4600000",
+      discount: "3500000",
+    },
+    {
+      id: 10,
+      name: "Lồng Chào Mào Chân Trụ",
+      descriptions: "CG 01500",
+      image:
+        "https://longchim.vn/wp-content/uploads/2023/05/long-chao-mao-chan-tru-1-247x296.jpg",
+      rating: 5,
+      price: "17000000",
+      discount: "15000000",
+    },
+    {
+      id: 11,
+      name: "Lồng Chào Mào Gỗ Mun Hoa",
+      descriptions: "CG 0850",
+      image:
+        "https://longchim.vn/wp-content/uploads/2023/05/long-chao-mao-go-mun-hoa-1-247x296.jpg",
+      rating: 5,
+      price: "1100000",
+      discount: "900000",
+    },
+    {
+      id: 12,
+      name: "Lồng Chào Mào Gỗ Mun Sọc Chân Chạm",
+      descriptions: "CG350",
+      image:
+        "https://longchim.vn/wp-content/uploads/2023/05/long-chao-mao-go-mun-soc-chan-cham-1-247x296.jpg",
+      rating: 3,
+      price: "450000",
+      discount: "370000",
+    },
+    {
+        id: 13,
+        name: "Lồng Chào Mào Sin Tre Già",
+        descriptions: "CG350",
+        image:
+          "https://longchim.vn/wp-content/uploads/2023/05/long-chao-mao-sin-tre-gia-1-247x296.jpg",
+        rating: 3,
+        price: "450000",
+        discount: "3700000",
+    },
+    {
+        id: 14,
+        name: "Lồng Chào Mào Tang Lồi Cắt Góc",
+        descriptions: "CG350",
+        image:
+          "https://longchim.vn/wp-content/uploads/2023/05/long-chao-mao-tang-loi-cat-goc-1-247x296.jpg",
+        rating: 3,
+        price: "450000",
+        discount: "370000",
+    },
+    {
+        id: 15,
+        name: "Lồng Chào Mào Tang Lồi Vuông",
+        descriptions: "CG350",
+        image:
+          "https://longchim.vn/wp-content/uploads/2023/05/long-chao-mao-tang-loi-vuong-1-247x296.jpg",
+        rating: 3,
+        price: "370000",
+        discount: "450000",
+    },{
+        id: 16,
+        name: "Lồng Chào Mào Tre Già Nan Khít",
+        descriptions: "CG350",
+        image:
+          "https://longchim.vn/wp-content/uploads/2023/05/long-chao-mao-tre-gia-nan-khit-1-247x296.jpg",
+        rating: 3,
+        price: "370000",
+        discount: "450000",
+    },
+	
+  ];
