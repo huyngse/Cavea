f7330782e9f206681ef6c15236f492653ab70d9f
levquang <138362246+levquang@users.noreply.github.com>
2023-10-01 21:45:52 +0700

    update more database list-product

---
+++ src/Components/List-cart/list_product_khuyen.jsx
@@ -0,0 +1,160 @@
+export const products_khuyen = [
+    {
+      id: 1,
+      name: "Lồng Chim Bọc Sừng Bò Chạm Bi Tùng Nai Thủng",
+      descriptions: "CG 0350",
+      image:
+        "https://longchim.vn/wp-content/uploads/2023/05/long-chim-boc-sung-bo-cham-bi-tung-nai-thung-1-247x296.jpg",
+      rating: 4,
+      price: "4600000",
+      discount: "3500000",
+    },
+    {
+      id: 2,
+      name: "Lồng Chim Cao Độ Sừng Bò Châu Phi Chạm Tùng Hạc Viền Bi",
+      descriptions: "CG 01500",
+      image:
+        "https://longchim.vn/wp-content/uploads/2023/05/long-chim-do-sung-bo-chau-phi-cham-tung-hac-vien-bi-3-247x296.jpg",
+      rating: 5,
+      price: "17000000",
+      discount: "15000000",
+    },
+    {
+      id: 3,
+      name: "Lồng Khuyên Chạm Tay Lãng Hoa Thủng",
+      descriptions: "CG 0850",
+      image:
+        "https://longchim.vn/wp-content/uploads/2023/05/long-khuyen-cham-tay-lang-hoa-thung-1-247x296.jpg",
+      rating: 5,
+      price: "1100000",
+      discount: "900000",
+    },
+    {
+      id: 4,
+      name: "Lồng Khuyên Gỗ Mun Chạm Tùng Nai Thủng",
+      descriptions: "CG350",
+      image:
+        "https://longchim.vn/wp-content/uploads/2023/05/long-khuyen-tre-gia-do-mun-sung-cham-tung-nai-thung-1-247x296.jpg",
+      rating: 3,
+      price: "450000",
+      discount: "370000",
+    },
+    {
+        id: 5,
+        name: "Lồng Khuyên Lá Đề Tre Già Đế Trơn ST4011",
+        descriptions: "CG350",
+        image:
+          "https://longchim.vn/wp-content/uploads/2023/05/long-la-de-1-247x296.jpg",
+        rating: 3,
+        price: "450000",
+        discount: "3700000",
+    },
+    {
+        id: 6,
+        name: "Lồng Khuyên Mái Vòm Gỗ Mun Nan Tre ST4010",
+        descriptions: "CG350",
+        image:
+          "https://longchim.vn/wp-content/uploads/2023/05/long-khuyen-vuong-mai-vom-go-mun-nan-tre-1-247x296.jpg",
+        rating: 3,
+        price: "450000",
+        discount: "370000",
+    },
+    {
+        id: 7,
+        name: "Lồng Khuyên Tàu Dừa Trúc Già ST4012",
+        descriptions: "CG350",
+        image:
+          "https://longchim.vn/wp-content/uploads/2023/05/long-khuyen-dua-1-247x296.jpg",
+        rating: 3,
+        price: "370000",
+        discount: "450000",
+    },{
+        id: 8,
+        name: "Lồng Khuyên Tàu Lùn Mini Nhỏ Kiểu Cách Trung Quốc Độ Mun",
+        descriptions: "CG350",
+        image:
+          "https://longchim.vn/wp-content/uploads/2023/05/long-khuyen-tre-gia-cham-truc-ho-phu-do-mun-1-247x296.jpg",
+        rating: 3,
+        price: "370000",
+        discount: "450000",
+    },  {
+      id: 9,
+      name: "Lồng Chim Bọc Sừng Bò Chạm Bi Tùng Nai Thủng",
+      descriptions: "CG 0350",
+      image:
+        "https://longchim.vn/wp-content/uploads/2023/05/long-chim-boc-sung-bo-cham-bi-tung-nai-thung-1-247x296.jpg",
+      rating: 4,
+      price: "4600000",
+      discount: "3500000",
+    },
+    {
+      id: 10,
+      name: "Lồng Chim Cao Độ Sừng Bò Châu Phi Chạm Tùng Hạc Viền Bi",
+      descriptions: "CG 01500",
+      image:
+        "https://longchim.vn/wp-content/uploads/2023/05/long-chim-do-sung-bo-chau-phi-cham-tung-hac-vien-bi-3-247x296.jpg",
+      rating: 5,
+      price: "17000000",
+      discount: "15000000",
+    },
+    {
+      id: 11,
+      name: "Lồng Khuyên Chạm Tay Lãng Hoa Thủng",
+      descriptions: "CG 0850",
+      image:
+        "https://longchim.vn/wp-content/uploads/2023/05/long-khuyen-cham-tay-lang-hoa-thung-1-247x296.jpg",
+      rating: 5,
+      price: "1100000",
+      discount: "900000",
+    },
+    {
+      id: 12,
+      name: "Lồng Khuyên Gỗ Mun Chạm Tùng Nai Thủng",
+      descriptions: "CG350",
+      image:
+        "https://longchim.vn/wp-content/uploads/2023/05/long-khuyen-tre-gia-do-mun-sung-cham-tung-nai-thung-1-247x296.jpg",
+      rating: 3,
+      price: "450000",
+      discount: "370000",
+    },
+    {
+        id: 13,
+        name: "Lồng Khuyên Lá Đề Tre Già Đế Trơn ST4011",
+        descriptions: "CG350",
+        image:
+          "https://longchim.vn/wp-content/uploads/2023/05/long-la-de-1-247x296.jpg",
+        rating: 3,
+        price: "450000",
+        discount: "3700000",
+    },
+    {
+        id: 14,
+        name: "Lồng Khuyên Mái Vòm Gỗ Mun Nan Tre ST4010",
+        descriptions: "CG350",
+        image:
+          "https://longchim.vn/wp-content/uploads/2023/05/long-khuyen-vuong-mai-vom-go-mun-nan-tre-1-247x296.jpg",
+        rating: 3,
+        price: "450000",
+        discount: "370000",
+    },
+    {
+        id: 15,
+        name: "Lồng Khuyên Tàu Dừa Trúc Già ST4012",
+        descriptions: "CG350",
+        image:
+          "https://longchim.vn/wp-content/uploads/2023/05/long-khuyen-dua-1-247x296.jpg",
+        rating: 3,
+        price: "370000",
+        discount: "450000",
+    },{
+        id: 16,
+        name: "Lồng Khuyên Tàu Lùn Mini Nhỏ Kiểu Cách Trung Quốc Độ Mun",
+        descriptions: "CG350",
+        image:
+          "https://longchim.vn/wp-content/uploads/2023/05/long-khuyen-tre-gia-cham-truc-ho-phu-do-mun-1-247x296.jpg",
+        rating: 3,
+        price: "370000",
+        discount: "450000",
+    },
+	
+  ];
