e33ee62d1c90261edef7c1be854bb2ae66d40828
levquang <138362246+levquang@users.noreply.github.com>
2023-10-01 15:30:47 +0700

    update more database list-product

---
+++ src/Components/List-cart/list_product_hoami.jsx
@@ -0,0 +1,160 @@
+export const products_hoami = [
+    {
+      id: 1,
+      name: "Lồng Chim 68 Nan Chân Lá Đề",
+      descriptions: "CG 0350",
+      image:
+        "https://longchim.vn/wp-content/uploads/2023/05/long-chim-68-nan-chan-la-de-1-247x296.jpg",
+      rating: 4,
+      price: "4600000",
+      discount: "3500000",
+    },
+    {
+      id: 2,
+      name: "Lồng Cửa Kiếm Cách Tân",
+      descriptions: "CG 01500",
+      image:
+        "https://longchim.vn/wp-content/uploads/2023/05/long-cua-kiem-cach-tan-1-247x296.jpg",
+      rating: 5,
+      price: "17000000",
+      discount: "15000000",
+    },
+    {
+      id: 3,
+      name: "Lồng Họa Mi Phối Chạm Cao Cấp",
+      descriptions: "CG 0850",
+      image:
+        "https://longchim.vn/wp-content/uploads/2023/05/long-hoa-mi-phoi-cham-1-247x296.jpg",
+      rating: 5,
+      price: "1100000",
+      discount: "900000",
+    },
+    {
+      id: 4,
+      name: "Lồng Mi Tàu Tre Già Chân Lá Đề Nam 60",
+      descriptions: "CG350",
+      image:
+        "https://longchim.vn/wp-content/uploads/2023/05/long-hoa-mi-56-60-nan-full-set-2-247x296.jpg",
+      rating: 3,
+      price: "450000",
+      discount: "370000",
+    },
+    {
+        id: 5,
+        name: "Lồng Mun Chạm Hoa Mai",
+        descriptions: "CG350",
+        image:
+          "https://longchim.vn/wp-content/uploads/2023/05/long-mun-cham-hoa-mai-1-247x296.jpg",
+        rating: 3,
+        price: "450000",
+        discount: "3700000",
+    },
+    {
+        id: 6,
+        name: "Lồng Mun Cửa Kiếm Cao Cấp ST3008",
+        descriptions: "CG350",
+        image:
+          "https://longchim.vn/wp-content/uploads/2023/05/long-mun-cua-kiem-cao-cap-1-247x296.jpg",
+        rating: 3,
+        price: "450000",
+        discount: "370000",
+    },
+    {
+        id: 7,
+        name: "Lồng Tre Già Chạm Hoa Mai Tinh Xảo",
+        descriptions: "CG350",
+        image:
+          "https://longchim.vn/wp-content/uploads/2023/05/long-mun-cua-kiem-cao-cap-1-247x296.jpg",
+        rating: 3,
+        price: "370000",
+        discount: "450000",
+    },{
+        id: 8,
+        name: "Lồng Tre Già Phối Mun Chân Trơn/Chạm Cao Cấp",
+        descriptions: "CG350",
+        image:
+          "https://longchim.vn/wp-content/uploads/2023/05/long-khuou-phoi-mun-cao-cap-2-247x296.jpg",
+        rating: 3,
+        price: "370000",
+        discount: "450000",
+    },{
+      id: 9,
+      name: "Lồng Chim 68 Nan Chân Lá Đề",
+      descriptions: "CG 0350",
+      image:
+        "https://longchim.vn/wp-content/uploads/2023/05/long-chim-68-nan-chan-la-de-1-247x296.jpg",
+      rating: 4,
+      price: "4600000",
+      discount: "3500000",
+    },
+    {
+      id: 10,
+      name: "Lồng Cửa Kiếm Cách Tân",
+      descriptions: "CG 01500",
+      image:
+        "https://longchim.vn/wp-content/uploads/2023/05/long-cua-kiem-cach-tan-1-247x296.jpg",
+      rating: 5,
+      price: "17000000",
+      discount: "15000000",
+    },
+    {
+      id: 11,
+      name: "Lồng Họa Mi Phối Chạm Cao Cấp",
+      descriptions: "CG 0850",
+      image:
+        "https://longchim.vn/wp-content/uploads/2023/05/long-hoa-mi-phoi-cham-1-247x296.jpg",
+      rating: 5,
+      price: "1100000",
+      discount: "900000",
+    },
+    {
+      id: 12,
+      name: "Lồng Mi Tàu Tre Già Chân Lá Đề Nam 60",
+      descriptions: "CG350",
+      image:
+        "https://longchim.vn/wp-content/uploads/2023/05/long-hoa-mi-56-60-nan-full-set-2-247x296.jpg",
+      rating: 3,
+      price: "450000",
+      discount: "370000",
+    },
+    {
+        id: 13,
+        name: "Lồng Mun Chạm Hoa Mai",
+        descriptions: "CG350",
+        image:
+          "https://longchim.vn/wp-content/uploads/2023/05/long-mun-cham-hoa-mai-1-247x296.jpg",
+        rating: 3,
+        price: "450000",
+        discount: "3700000",
+    },
+    {
+        id: 14,
+        name: "Lồng Mun Cửa Kiếm Cao Cấp ST3008",
+        descriptions: "CG350",
+        image:
+          "https://longchim.vn/wp-content/uploads/2023/05/long-mun-cua-kiem-cao-cap-1-247x296.jpg",
+        rating: 3,
+        price: "450000",
+        discount: "370000",
+    },
+    {
+        id: 15,
+        name: "Lồng Tre Già Chạm Hoa Mai Tinh Xảo",
+        descriptions: "CG350",
+        image:
+          "https://longchim.vn/wp-content/uploads/2023/05/long-mun-cua-kiem-cao-cap-1-247x296.jpg",
+        rating: 3,
+        price: "370000",
+        discount: "450000",
+    },{
+        id: 16,
+        name: "Lồng Tre Già Phối Mun Chân Trơn/Chạm Cao Cấp",
+        descriptions: "CG350",
+        image:
+          "https://longchim.vn/wp-content/uploads/2023/05/long-khuou-phoi-mun-cao-cap-2-247x296.jpg",
+        rating: 3,
+        price: "370000",
+        discount: "450000",
+    }
+	
+  ];
